39408b0e2295e1dd3344af6a0348240f78c010d0
Noel Rocha Morais <noelrocha@gmail.com>
2009-01-26 03:59:53 -0200

    added gem composite_primary_keys

---
+++ vendor/gems/composite_primary_keys-2.2.1/website/version-raw.js
@@ -0,0 +1,3 @@
+// Announcement JS file
+var version = "2.2.1";
+MagicAnnouncement.show('compositekeys', version);
---
+++ vendor/gems/composite_primary_keys-2.2.1/website/version.js
@@ -0,0 +1,4 @@
+// Version JS file
+var version = "2.2.1";
+
+document.write(" - " + version);
