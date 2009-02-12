59cb5051d4ffeea817d341d127c40e8d0fe904a3
Noel Rocha <noelrocha@localhost.(none)>
2009-02-12 19:57:33 -0200

    removed freezed rails

--- vendor/rails/railties/configs/routes.rb
+++
@@ -1,6 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "subscribers"
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-end
